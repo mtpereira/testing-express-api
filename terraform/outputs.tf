21db39e122dc7de178d77f83bf26571ed41fb17e
Manuel Tiago Pereira <mt.pereira@gmail.com>
2017-12-03 17:39:11 +0000

    Add useful outputs to terraform.

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,11 @@
 output "elb_address" {
   value = "${aws_elb.default.dns_name}"
 }
+
+output "ami_id" {
+  value = "${data.aws_ami.instances.id}"
+}
+
+output "app_version" {
+  value = "${var.app_version}"
+}
